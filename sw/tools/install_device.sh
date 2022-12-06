7a9d87df9d0b81109dcc87c18a712fa5d28b3598
Heehoon Kim <heehoon@aces.snu.ac.kr>
2022-12-06 07:35:14 +0000

    chmod reset during install

--- sw/tools/install_device.sh
+++ sw/tools/install_device.sh
@@ -6,3 +6,4 @@ cd $SCRIPT_DIR/../driver/dma_ip_drivers/XDMA/linux-kernel/xdma
 sudo sh -c "make install"
 sudo sh -c "echo 1 > /sys/bus/pci/rescan"
 sudo sh -c "chmod o+rw /dev/xdma0_user"
+sudo sh -c "chmod o+w /sys/bus/pci/devices/0000:86:00.0/reset"
