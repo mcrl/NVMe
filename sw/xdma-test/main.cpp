ce966f7b9d6b0f61f6bc79edada5c16e0fc69ace
sota-junsik <bbohae@snu.ac.kr>
2022-12-31 07:40:58 +0000

    revised read opcode

--- sw/xdma-test/main.cpp
+++ sw/xdma-test/main.cpp
@@ -474,7 +474,7 @@ void NVMePrepReadCommand(size_t nvme_addr, size_t fpga_addr, size_t data_length)
   
   // merge opcode and data length
   size_t dataLength_Opcode = data_length << 16;
-  dataLength_Opcode += 0x1; // Write Opcode 0x1
+  dataLength_Opcode += 0x2; // Write Opcode 0x1
   
 }
 
