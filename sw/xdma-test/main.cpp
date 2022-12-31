f7baebb973de8daac8ced66ce87b495701468c9a
sota-junsik <bbohae@snu.ac.kr>
2022-12-31 08:48:20 +0000

    check debugging messages of o2k_ar

--- sw/xdma-test/main.cpp
+++ sw/xdma-test/main.cpp
@@ -551,7 +551,7 @@ void NVMeWriteDoorbell(){
   uint32_t arlen = (KernelRead(0x08) >> 3) & 0xff;
   uint32_t arid = (KernelRead(0x08) >> 11) & 0b1111;
   
-  //spdlog::info("[CQ] o2k_ar araddr={:08X},{:08X} arsize={}, arlen={}, arid={}", araddr1, araddr0, arsize, arlen, arid);
+  spdlog::info("[CQ] o2k_ar araddr={:08X},{:08X} arsize={}, arlen={}, arid={}", araddr1, araddr0, arsize, arlen, arid);
 
 
 }
