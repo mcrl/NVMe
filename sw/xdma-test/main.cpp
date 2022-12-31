442dab4ce055fa36a173449061cdf0690ceb831d
sota-junsik <bbohae@snu.ac.kr>
2022-12-31 08:56:12 +0000

    add debugging messages

--- sw/xdma-test/main.cpp
+++ sw/xdma-test/main.cpp
@@ -551,8 +551,8 @@ void NVMeWriteDoorbell(){
   uint32_t arlen = (KernelRead(0x08) >> 3) & 0xff;
   uint32_t arid = (KernelRead(0x08) >> 11) & 0b1111;
   
-  spdlog::info("[CQ] o2k_ar araddr={:08X},{:08X} arsize={}, arlen={}, arid={}", araddr1, araddr0, arsize, arlen, arid);
-
+  spdlog::info("NVMe Request SQ");
+  spdlog::info("o2k_ar araddr={:08X},{:08X} arsize={}, arlen={}, arid={}", araddr1, araddr0, arsize, arlen, arid);
 
 }
 
