b1ff00601ecc48bf8c6d0dd0c2f30d7bae8f53a7
sota-junsik <bbohae@snu.ac.kr>
2023-04-06 11:18:58 -0700

    add minor modules + Add line: modification, are permitted provided that the following conditions are met:

--- hw/RTL/fpga-nvme-remote-driver/template_top.v
+++ hw/RTL/fpga-nvme-remote-driver/template_top.v
@@ -3,3 +3,4 @@ Copyright (c) 2021, Xilinx, Inc.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
