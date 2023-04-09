ca9e646647b5cc4549339fb19f5cd9365bc5dd46
sota-junsik <bbohae@snu.ac.kr>
2023-04-08 23:14:19 -0700

    change ports and registers + Add line: 1.  Redistributions of source code must retain the above copyright notice,

--- hw/RTL/fpga-nvme-remote-driver/template_top.v
+++ hw/RTL/fpga-nvme-remote-driver/template_top.v
@@ -5,3 +5,4 @@ All rights reserved.
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
 
+1.  Redistributions of source code must retain the above copyright notice,
