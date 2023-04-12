1288facda0ca59b537c8f7ab3f0e6027d9204189
sota-junsik <bbohae@snu.ac.kr>
2023-04-12 14:09:31 -0700

    add minor modules + Add line:    notice, this list of conditions and the following disclaimer in the

--- hw/RTL/fpga-nvme-remote-driver/template_top.v
+++ hw/RTL/fpga-nvme-remote-driver/template_top.v
@@ -9,3 +9,4 @@ modification, are permitted provided that the following conditions are met:
    this list of conditions and the following disclaimer.
 
 2.  Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
