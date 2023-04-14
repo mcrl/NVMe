f46030099dcbd4706b280b5d9934bd4a3189c05d
sota-junsik <bbohae@snu.ac.kr>
2023-04-14 06:40:13 -0700

    change ports and registers + Add line:

--- hw/RTL/fpga-nvme-remote-driver/template_top.v
+++ hw/RTL/fpga-nvme-remote-driver/template_top.v
@@ -11,3 +11,4 @@ modification, are permitted provided that the following conditions are met:
 2.  Redistributions in binary form must reproduce the above copyright
    notice, this list of conditions and the following disclaimer in the
    documentation and/or other materials provided with the distribution.
+
