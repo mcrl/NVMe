9b8a13760c6336fdeb0fff0d0b4cd42feb957e71
sota-junsik <bbohae@snu.ac.kr>
2023-05-31 06:20:40 -0700

    revised minor errors + Add line:   output wire     [AXIS_TDATA_WIDTH/8-1:0]    M_AXIS_tkeep,

--- hw/RTL/fpga-nvme-remote-driver/template_top.v
+++ hw/RTL/fpga-nvme-remote-driver/template_top.v
@@ -58,3 +58,4 @@ module placeholder #(
   output wire                                 M_AXIS_tvalid,
   input  wire                                 M_AXIS_tready,
   output wire       [AXIS_TDATA_WIDTH-1:0]    M_AXIS_tdata,
+  output wire     [AXIS_TDATA_WIDTH/8-1:0]    M_AXIS_tkeep,
