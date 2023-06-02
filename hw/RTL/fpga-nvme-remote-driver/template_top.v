e47868d949d4b03c186ffc8808647c5699d0f597
sota-junsik <bbohae@snu.ac.kr>
2023-06-01 18:20:27 -0700

    change ports and registers + Add line:   output wire                                 M_AXIS_tlast,

--- hw/RTL/fpga-nvme-remote-driver/template_top.v
+++ hw/RTL/fpga-nvme-remote-driver/template_top.v
@@ -59,3 +59,4 @@ module placeholder #(
   input  wire                                 M_AXIS_tready,
   output wire       [AXIS_TDATA_WIDTH-1:0]    M_AXIS_tdata,
   output wire     [AXIS_TDATA_WIDTH/8-1:0]    M_AXIS_tkeep,
+  output wire                                 M_AXIS_tlast,
