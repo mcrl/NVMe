dfec9af1025e5157af9155d10bf0013184a52559
sota-junsik <bbohae@snu.ac.kr>
2023-11-15 17:27:11 +0000

    clean up sw/

--- sw/driver/install_n_compile_driver.sh
+++
@@ -1,6 +0,0 @@
-XDMA_PATH=$(pwd)
-sudo sh -c "echo 1 > /sys/bus/pci/devices/0000\:86\:00.0/remove"
-sudo sh -c "echo 1 > /sys/bus/pci/rescan"
-cd $XDMA_PATH/dma_ip_drivers/XDMA/linux-kernel/xdma
-sudo sh -c "DEBUG=1 make install"
-sudo ../tests/load_driver.sh
